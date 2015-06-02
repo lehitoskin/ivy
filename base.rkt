ef67f55c036f3140d438faf0ed4cb5621368bbaa
Lehi Toskin <lehi@tosk.in>
2015-06-02 04:34:33 -0700

    Tag removal, image cycling, default image scaling
    On empty tag list, remove path-tag from dictionary.
    Load previous and next images in the directory by pressing the buttons.
    Scale image to the current canvas size when it is loaded and drawn.

--- base.rkt
+++ base.rkt
@@ -1,8 +1,17 @@
 #lang racket/base
 ; base.rkt
 ; base file for ivy, the taggable image viewer
-(require json racket/dict)
+(require json
+         racket/dict
+         racket/bool
+         racket/list)
 (provide (all-defined-out))
+
+(define (path->symbol p)
+  (string->symbol (path->string p)))
+
+(define (symbol->path p)
+  (string->path (symbol->string p)))
 
 ; master dictionary
 ; (absolute-file-path . '(sorted list of tags))
@@ -19,11 +28,34 @@
                                     "Library/Application Support/ivy")]))
 (define master-file (build-path ivy-path "catalog.json"))
 
-(define (save-dict dct)
+(define (save-dict! dct)
   (with-output-to-file master-file
     (λ () (write-json dct))
     #:exists 'truncate/replace
     #:mode 'text))
+
+(define (clear-dict! dct)
+  (for ([file-sym (in-dict-keys dct)])
+    (define file-path (symbol->path file-sym))
+    (printf "Checking ~s...~n" file-path)
+    (unless (file-exists? file-path)
+      (printf "Removing ~s from dictionary.~n" file-path)
+      (dict-remove! dct file-sym))))
+
+; enter a dictionary (master) and tag strings to search for.
+; returns a list of image paths or empty on failure
+(define (search-dict dct . items)
+  (define search-results
+    (flatten
+     (for/list ([(paths tags) (in-dict dct)])
+       (define result (map (λ (i) (member i tags)) items))
+       ; check for false through the result list
+       ; if not false, return the path for the result
+       (map (λ (l) (if (false? l) l paths)) result))))
+  ; filter out any false
+  (define filtered (filter symbol? search-results))
+  ; turn the symbols into paths and remove any duplicates
+  (remove-duplicates (map symbol->path filtered)))
 
 ; create the config directory
 (unless (directory-exists? ivy-path)
--- frame.rkt
+++ frame.rkt
@@ -1,40 +1,74 @@
 #lang racket/gui
 ; frame.rkt
 ; main frame file for ivy, the taggable image viewer
-(require "base.rkt")
+(require "base.rkt" pict)
 (provide (all-defined-out))
 
-(define (path->symbol p)
-  (string->symbol (path->string p)))
+; path of the currently displayed image
+(define image-path '/)
+; bitmap of the currently displayed image
+(define image-bmp (make-bitmap 50 50))
+; directory containing the currently displayed image
+(define image-dir (find-system-path 'home-dir))
+; all files contained within image-dir
+(define (path-files)
+  (directory-list image-dir #:build? #t))
 
-(define image-path (make-parameter '/))
-(define image-bmp (make-parameter (make-bitmap 50 50)))
-(define previous-path (find-system-path 'home-dir))
+; get index of an item in the list
+; numbering starts from 0
+(define (get-index item lst)
+  (define len (length lst))
+  (define pos (member item lst))
+  (if pos
+      (- len (length pos))
+      #f))
 
+; scales an image to the current canvas size
+(define (scale-image img)
+  ; width and height of the image
+  (define img-width (send img get-width))
+  (define img-height (send img get-height))
+  
+  ; width and height of the canvas
+  (define max-width (send ivy-canvas get-width))
+  (define max-height (send ivy-canvas get-height))
+  
+  (cond [(and (> img-width max-width)
+              (> img-height max-height))
+         (pict->bitmap (scale-to-fit (bitmap img) max-width max-height))]
+        [(> img-width max-width)
+         (pict->bitmap (scale-to-fit (bitmap img) max-width img-height))]
+        [(> img-height max-height)
+         (pict->bitmap (scale-to-fit (bitmap img) img-width max-height))]
+        [else img]))
+
+; procedure that loads the given image (from the path)
+; to the canvas
 (define (load-image path)
   (define-values (base name must-be-dir?) (split-path path))
-  (set! previous-path base)
-  (image-path (path->symbol path))
+  (set! image-dir base)
+  (set! image-path (path->symbol path))
   ; make sure the bitmap loaded correctly
-  (define load-success (send (image-bmp) load-file path))
+  (define load-success (send image-bmp load-file path))
   (cond [load-success
+         (set! image-bmp (scale-image image-bmp))
          ; if we've set tags for this file before...
-         (cond [(hash-has-key? master (image-path))
+         (cond [(hash-has-key? master image-path)
                 (define tag
-                  (string-join (hash-ref master (image-path)) ","))
+                  (string-join (hash-ref master image-path) ","))
                 ; ...put them in the tfield
                 (send ivy-tag-tfield set-value tag)]
                ; ...otherwise clear the tfield
                [else (send ivy-tag-tfield set-value "")])
          
-         (define width (send (image-bmp) get-width))
-         (define height (send (image-bmp) get-height))
+         (define width (send image-bmp get-width))
+         (define height (send image-bmp get-height))
          (send ivy-canvas set-on-paint!
                (λ ()
                  (send ivy-canvas set-canvas-background
                        (make-object color% "black"))
                  (send (send ivy-canvas get-dc) draw-bitmap
-                       (image-bmp) 0 0)))
+                       image-bmp 0 0)))
          (send ivy-canvas init-auto-scrollbars width height 0.0 0.0)
          (send ivy-canvas refresh)]
         [else (printf "Error loading file ~a~n" path)]))
@@ -51,15 +85,6 @@
                                [parent ivy-menu-bar]
                                [label "&File"]))
 
-#|
-; turn the bitmap into a pict
-(define icon-pict (bitmap icon-bitmap))
-; scale the pict to 40x40
-(define icon-pict-small (scale-to-fit icon-pict 40 40))
-; set avatar parameter to our scaled icon
-(my-avatar (pict->bitmap icon-pict-small))
-|#
-
 (define ivy-menu-bar-file-open
   (new menu-item%
        [parent ivy-menu-bar-file]
@@ -69,9 +94,33 @@
        [callback (λ (i e)
                    (define path (get-file "Select an image to view."
                                           #f
-                                          previous-path))
-                   ; make sure the path is a real path
+                                          image-dir))
+                   ; make sure the path is not false
                    (when path (load-image path)))]))
+
+(define ivy-menu-bar-search-tag
+  (new menu-item%
+       [parent ivy-menu-bar-file]
+       [label "&Search Tags"]
+       [help-string "Search tags for an image."]
+       [callback (λ (i e)
+                   (define search-tag-dialog
+                     (new dialog%
+                          [label "Ivy - Search Tags"]
+                          [width 200]
+                          [height 100]
+                          [style '(close-button)]))
+                   (define cancel-button
+                     (new button%
+                          [parent search-tag-dialog]
+                          [callback (λ (button event)
+                                      (send search-tag-dialog show #f))]))
+                   (define ok-button
+                     (new button%
+                          [parent search-tag-dialog]
+                          [callback (λ (button event)
+                                      (send search-tag-dialog show #f))]))
+                   (send search-tag-dialog show #t))]))
 
 (define ivy-menu-bar-file-quit
   (new menu-item%
@@ -81,7 +130,42 @@
        [help-string "Quit the program."]
        [callback (λ (i e) (exit))]))
 
-(define ivy-tag-hpanel (new horizontal-panel% [parent ivy-frame]))
+; left/right, zoom in/out
+(define actions-hpanel (new horizontal-panel%
+                            [parent ivy-frame]
+                            [alignment '(center center)]
+                            [stretchable-height #f]))
+
+(define ivy-action-previous
+  (new button%
+       [parent actions-hpanel]
+       [label (pict->bitmap (arrow 15 pi))]
+       [callback (λ (button event)
+                   (unless (eq? image-path '/)
+                     (define pfs (path-files))
+                     (define index (get-index (symbol->path image-path) pfs))
+                     (when (and index (> (length pfs) 1))
+                       (if (zero? index)
+                           (load-image (last pfs))
+                           (load-image (list-ref pfs (- index 1)))))))]))
+
+(define ivy-actions-next
+  (new button%
+       [parent actions-hpanel]
+       [label (pict->bitmap (arrow 15 0))]
+       [callback (λ (button event)
+                   (unless (eq? image-path '/)
+                     (define pfs (path-files))
+                     (define index (get-index (symbol->path image-path) pfs))
+                     (when (and index (> (length pfs) 1))
+                       (if (= index (- (length pfs) 1))
+                           (load-image (first pfs))
+                           (load-image (list-ref pfs (+ index 1)))))))]))
+
+
+(define ivy-tag-hpanel (new horizontal-panel%
+                            [parent ivy-frame]
+                            [stretchable-height #f]))
 
 ; list of tags separated by commas
 ; e.g. flower,castle,too many cooks,fuzzy wuzzy wuz a bear,etc
@@ -99,13 +183,13 @@
                    ; empty tag string means delete the entry
                    (cond [(string=? tag "")
                           ; no failure if key doesn't exist
-                          (dict-remove! master (image-path))]
-                         [(not (eq? (image-path) '/))
+                          (dict-remove! master image-path)]
+                         [(not (eq? image-path '/))
                           ; turn the string of tag(s) into a list then sort it
                           (define tag-lst (sort (string-split tag ",") string<?))
                           ; set and save the dictionary
-                          (dict-set! master (image-path) tag-lst)
-                          (save-dict master)]))]))
+                          (dict-set! master image-path tag-lst)
+                          (save-dict! master)]))]))
 
 (define ivy-canvas%
   (class canvas%
@@ -126,7 +210,6 @@
        [parent ivy-frame]
        [label "Ivy Image Canvas"]
        [style '(hscroll vscroll)]
-       [min-height 550]
        [paint-callback (λ (canvas dc)
                          (send canvas set-canvas-background
                                (make-object color% "black")))]))
