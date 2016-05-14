5e9820edfd97e8c556a1d2e06e3d71b573a69adf
Brigham Toskin <brigham@ionoclast.com>
2016-05-13 22:34:10 -0700

    New navigation menus.
    New View menu with navigation items.
    Move Random into View menu.
    New Window menu with Minimize shortcut.

--- frame.rkt
+++ frame.rkt
@@ -23,6 +23,16 @@
                                [parent ivy-menu-bar]
                                [label "&File"]))
 
+(define ivy-menu-bar-view (new menu%
+                               [parent ivy-menu-bar]
+                               [label "&View"]))
+
+(define ivy-menu-bar-window (new menu%
+                                 [parent ivy-menu-bar]
+                                 [label "&Window"]))
+
+;; File menu items ;;
+
 ; opening a single image will have the current directory
 ; contents be the collection
 (define ivy-menu-bar-file-open
@@ -147,6 +157,56 @@
            [help-string "Quit the program."]
            [callback (λ (i e) (exit))])))
 
+;; View menu items ;;
+
+(define ivy-menu-bar-view-prev
+  (new menu-item%
+       [parent ivy-menu-bar-view]
+       [label "Previous Image"]
+       [help-string "Display the Previous Image."]
+       [callback (λ (i e) (load-previous-image))]))
+
+(define ivy-menu-bar-view-next
+  (new menu-item%
+       [parent ivy-menu-bar-view]
+       [label "Next Image"]
+       [help-string "Display the Next Image."]
+       [callback (λ (i e) (load-next-image))]))
+
+(define ivy-menu-bar-view-first
+  (new menu-item%
+       [parent ivy-menu-bar-view]
+       [label "First Image"]
+       [help-string "Display the First Image."]
+       [callback (λ (i e) (load-first-image))]))
+
+(define ivy-menu-bar-view-last
+  (new menu-item%
+       [parent ivy-menu-bar-view]
+       [label "Last Image"]
+       [help-string "Display the Last Image."]
+       [callback (λ (i e) (load-last-image))]))
+
+(define ivy-menu-bar-view-rand
+  (new menu-item%
+       [parent ivy-menu-bar-view]
+       [label "&Random Image"]
+       [shortcut #\R]
+       [help-string "Display a Random Image."]
+       [callback (λ (i e) (load-rand-image))]))
+
+;; Window menu items ;;
+
+(define ivy-menu-bar-window-minimize
+  (new menu-item%
+       [parent ivy-menu-bar-window]
+       [label "&Minimize"]
+       [shortcut #\M]
+       [help-string "Minimize the Window."]
+       [callback (λ (i e) (send ivy-frame iconize #t))]))
+
+;; main window layout ;;
+
 ; left/right, zoom in/out,
 ; list of tags separated by commas
 ; e.g. flower,castle,too many cooks,fuzzy wuzzy wuz a bear,etc
@@ -160,13 +220,6 @@
        [label (pict->bitmap (arrow 15 pi))]
        [callback (λ (button event)
                    (load-previous-image))]))
-
-(define ivy-actions-random
-  (new button%
-       [parent ivy-toolbar-hpanel]
-       [label (pict->bitmap (arrowhead 15 (/ pi 2.0)))]
-       [callback (λ (button event)
-                   (load-rand-image))]))
 
 (define ivy-actions-next
   (new button%
@@ -319,7 +372,6 @@
            (load-image image-pict 'larger))]
         [(left) (load-previous-image)]
         [(right) (load-next-image)]
-        [(up) (load-rand-image)]
         [(home) (load-first-image)]
         [(end) (load-last-image)]
         [(#\return) (focus-tag-tfield)]))))
